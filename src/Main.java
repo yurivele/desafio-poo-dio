280b65c299312d57f1a19d743fa53d1bebfa5d49
Yuri Veras Leandro <yurivl@hotmail.com>
2022-08-09 21:22:48 -0300

    Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,29 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.Curso;
+import br.com.dio.desafio.Mentoria;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Curso curso1 = new Curso();
+		curso1.setTitulo("Curso Java");
+		curso1.setDescricao("descrição curso java");
+		curso1.setCargaHoraria(8);
+		
+		Curso curso2 = new Curso();
+		curso1.setTitulo("Curso js");
+		curso1.setDescricao("descrição curso js");
+		curso1.setCargaHoraria(4);
+		
+		Mentoria mentoria = new Mentoria();
+		mentoria.setTitulo("mentoria java");
+		mentoria.setDescricao("descrição mentoria java");
+		mentoria.setData(LocalDate.now());
+		
+		System.out.println(curso1);
+		System.out.println(curso2);
 
 	}
 
